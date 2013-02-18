cd22733138be4fae2438698d74b28df63494fedb
COSSON Mathieu <cosson.mathieu2@gmail.Com>
2013-02-18 09:57:17 +0100

    Fenetrer 0.1

--- src/GestionnaireTournois.java
+++ src/GestionnaireTournois.java
@@ -1,6 +1,9 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 
 public class GestionnaireTournois {
@@ -37,6 +40,22 @@ public class GestionnaireTournois {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton btnGestionnaireDarbitres = new JButton("Gestionnaire d'arbitres");
+		btnGestionnaireDarbitres.setBounds(42, 179, 160, 23);
+		frame.getContentPane().add(btnGestionnaireDarbitres);
+		
+		JButton btnGrerLesquipes = new JButton("G\u00E9rer les \u00E9quipes");
+		btnGrerLesquipes.setBounds(42, 56, 160, 23);
+		frame.getContentPane().add(btnGrerLesquipes);
+		
+		JButton btnGrerLesPoules = new JButton("G\u00E9rer les poules");
+		btnGrerLesPoules.setBounds(42, 101, 160, 23);
+		frame.getContentPane().add(btnGrerLesPoules);
+		
+		JButton btnGrerLesTournois = new JButton("G\u00E9rer les tournois");
+		btnGrerLesTournois.setBounds(42, 145, 160, 23);
+		frame.getContentPane().add(btnGrerLesTournois);
 	}
-
 }
